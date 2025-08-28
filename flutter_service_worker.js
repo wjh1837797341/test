5e44c22e896102d04ab2675a83e99c1901f5526b
wjh <1837797341@qq.com>
2025-08-28 11:32:59 +0800

    release build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "588414c5a32fb54d2f65a019725580a9
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "fa8384b14e65152e714f6bd1651e9025",
+"flutter_bootstrap.js": "4fe08aac05b276dbbbcdac33455f87a9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
